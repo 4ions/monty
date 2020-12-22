f05ad3ee2d34dfe5e83286d313c429ca2aa3dc18
4ions <leovalenciasantana@gmail.com>
2020-12-22 02:54:12 +0000

    nop

--- get_op.c
+++ get_op.c
@@ -20,6 +20,7 @@ void (*get_function(void))(stack_t **stack, unsigned int line_number)
 		{"pop", _pop},
 		{"swap", _swap},
 		{"add", _add},
+		{"nop", _nop},
 		{NULL, NULL}
 	};
 
---
+++ more_functions.c
@@ -0,0 +1,12 @@
+/**
+ * _nop - does not print, do not pass go, do not collect $200
+ * @stack: the stack
+ * @line_number: the line number
+ *
+ * Return: void
+ */
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
